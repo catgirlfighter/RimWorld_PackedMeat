928f5c31375b107ca61b1c2e0d5277100bbbed4f
CGFighter <catgirlfighter@gmail.com>
2021-07-08 02:14:34 +0300

    1.0.9

---
+++ Source/PackedMeat13/PackedMeat/ButcherCounter.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using Verse;
+using RimWorld;
+
+namespace PackedMeat
+{
+        public class avRecipeWorkerCounter_ButcherAnimals: RecipeWorkerCounter_ButcherAnimals
+    {
+            public static int countEachCat(Bill_Production bill, ThingCategoryDef cat)
+            {
+                int num = 0;
+                foreach(var i in (cat.childThingDefs))
+                    num += bill.Map.resourceCounter.GetCount(i);
+                foreach (var i in (cat.childCategories))
+                    num += countEachCat(bill, i);
+                return num;
+            }
+
+            public override int CountProducts(Bill_Production bill)
+            {
+                return countEachCat(bill, ThingCategoryDefOf.MeatRaw);
+            }
+        }
+}
---
+++ Source/PackedMeat13/PackedMeat/Settings.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace PackedMeat
+{
+    [DefOf]
+    public static class PackedMeatDefOf
+    {
+        public static ThingDef avRegularMeatPack;
+        public static ThingDef avOddMeatPack;
+        public static ThingDef avMysteriousMeatPack;
+    }
+
+    public class Settings : ModSettings
+    {
+        public static bool pack_on_butchering = false;
+        public static bool unusual_is_generic = false;
+        private static bool CSLoaded = false;
+        private static ModContentPack fCommonSense = null;
+        public static ModContentPack CommonSenseMod
+        {
+            get
+            {
+                if (CSLoaded)
+                    return fCommonSense;
+                else
+                {
+                    fCommonSense = LoadedModManager.RunningMods.FirstOrDefault(x => x.Name == "Common Sense");
+                    CSLoaded = true;
+                    return fCommonSense;
+                }
+            }
+        }
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(inRect);
+            listing_Standard.CheckboxLabeled("pack_on_butchering".Translate(), ref pack_on_butchering);
+            listing_Standard.CheckboxLabeled("odd_is_fine".Translate(), ref unusual_is_generic);
+
+
+            listing_Standard.End();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look<bool>(ref pack_on_butchering, "pack_on_butchering", false, false);
+            Scribe_Values.Look<bool>(ref unusual_is_generic, "unusual_is_generic", false, false);
+        }
+    }
+}
